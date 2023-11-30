f69ea2b9eb2de1e90a8a8cb5bd0656b5c0a90090
rillala <orillalin@gmail.com>
2023-11-30 18:11:01 +0800

    鞋子數字 size-chart to btn

---
+++ product/product.js
@@ -0,0 +1,9 @@
+const buyNumber = document.getElementById("item-number");
+
+document.getElementById("plus").addEventListener("click", function () {
+  buyNumber.value++;
+});
+
+document.getElementById("minus").addEventListener("click", function () {
+  if (buyNumber.value > 1) buyNumber.value--;
+});
